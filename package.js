0423a0bb00a6aa8893398074495951d13efb54a2
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-03-23 15:52:30 -0300

    increment version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cereal:accounts-fitbit',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'Adds account support for Fitbit',
   // URL to the Git repository containing the source code for this package.
